f7b4ebbe5dc542e8281844d10fa5023c45d1f885
edmondmw <edmondmwang@gmail.com>
2017-10-24 20:37:05 -0700

    Added player controller. Imported sword asset

---
+++ CS214-Zombie-Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10.0f;
+    public float jumpForce = 10f;
+
+    Rigidbody rb;
+    bool grounded = true;
+
+	void Start ()
+    {
+        // Makes cursor disappear
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(0, jumpForce, 0);
+        }
+    }
+
+    void Update ()
+    {
+        float vertical = Input.GetAxis("Vertical") * speed;
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        vertical *= Time.deltaTime;
+        horizontal *= Time.deltaTime;
+
+        transform.Translate(horizontal, 0, vertical);
+
+        // Unlock the cursor when esc is pressed
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
+}
---
+++ CS214-Zombie-Game/Assets/Sword/Rotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+    
+	void Update () {
+        transform.Rotate(new Vector3(0f,1.5f,0f));
+	}
+}
