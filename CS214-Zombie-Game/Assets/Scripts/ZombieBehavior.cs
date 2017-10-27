f572829c46cbe60bb4a883459b0180c11a61ad9d
Jianbin <lijianbin00@gmail.com>
2017-10-27 00:39:57 -0700

    A Simple Zombie can walk and attack
    Something related to HP is to be implemented.

--- CS214-Zombie-Game/Assets/Scripts/ZombieBehavior.cs
+++ CS214-Zombie-Game/Assets/Scripts/ZombieBehavior.cs
@@ -12,55 +12,59 @@ public class ZombieBehavior : MonoBehaviour {
     public float maxAttackDistance = 5f;
     public float turnSpeed = 0.01f;
     public int hp=100;
+    public float disappearTime=0;
 
     private float distanceFromPlayer;
     private Animator anim;
     private Rigidbody rb;
     private int attackHash = Animator.StringToHash("isAttack");
+    private bool isDead;
+
 	// Use this for initialization
 	void Start () {
-        rb=GetComponent<Rigidbody>();
+        //rb=GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (hp <= 0)
-        {
-            Dead();
-        }else
-        {
-            playerPosition = player.transform.position;
-            distanceFromPlayer = Vector3.Distance(playerPosition, transform.position);
-
-            if (distanceFromPlayer <= detectableRange)
+        if (!isDead) {
+            if (hp <= 0)
             {
-                Search(playerPosition);
+                isDead = true;
+                Dead();
+            }
+            else
+            {
+                playerPosition = player.transform.position;
+                distanceFromPlayer = Vector3.Distance(playerPosition, transform.position);
+
+                if (distanceFromPlayer <= detectableRange)
+                {
+                    Search(playerPosition);
 
 
-                if (distanceFromPlayer <= attackDistance)
-                {
-                    Attack(true);
-                }
-                else if (distanceFromPlayer > maxAttackDistance)
-                {
-                    Attack(false);
-                    Move();
+                    if (distanceFromPlayer <= attackDistance)
+                    {
+                        Attack(true);
+                    }
+                    else if (distanceFromPlayer > maxAttackDistance)
+                    {
+                        Attack(false);
+                        Move();
+                    }
                 }
             }
-        }
-        
 
+
+        }
     }
 
     void Search(Vector3 objectPosition)
     {
         Vector3 direction = playerPosition - transform.position;
         direction.y = 0;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed);
-
-        
-        
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed);   
     }
 
     void Move()
@@ -73,9 +77,25 @@ public class ZombieBehavior : MonoBehaviour {
         anim.SetBool(attackHash,isAttack);
      }
 
+    //Play a random dead animation and destroy the zombie after disappearTime.
     void Dead()
     {
-        int deadStyle = Random.Range(1, 4);
-        anim.SetInteger("deadStyle", deadStyle);
+        int deadStyle = Random.Range(0, 3);
+
+        switch (deadStyle)
+        {
+            case 0:
+                anim.Play("left_fall");
+                break;
+            case 1:
+                anim.Play("right_fall");
+                break;
+            default:
+                anim.Play("back_fall");
+                break;
+
+        }
+        Destroy(gameObject, disappearTime);
+        
     }
 }
